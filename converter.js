9ff404c373194ca5de8195259f36dd8285914edf
Smudasay <35358241+Smudasay@users.noreply.github.com>
2019-11-01 22:44:57 +0700

    Add files via upload

---
+++ converter.js
@@ -0,0 +1,7 @@
+
+//1 THB = 0.03 US DOLLAR
+function converter(currency) {
+    if(typeof currency === 'number') return `$${(currency*0.03).toFixed(2)}`
+    else  return 'error'
+}
+module.exports = converter;
